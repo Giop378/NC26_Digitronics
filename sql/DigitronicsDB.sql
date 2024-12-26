122622ecd1d340dd71eb1fc11ff8d94c68e9ab9c
Giovanni Paolo Chierchia <g.chierchia8@studenti.unisa.it>
2024-12-26 16:57:01 +0100

    Struttura progetto
    Struttura del progetto con cartelle

---
+++ sql/DigitronicsDB.sql
@@ -0,0 +1,92 @@
+Drop database if exists Digitronics;
+create database Digitronics;
+use Digitronics;
+
+create table categoria (
+	nome varchar(50) not null primary key,
+    descrizione text not null,
+    immagine varchar(255));
+
+create table prodotto(
+	IdProdotto int(5) zerofill auto_increment primary key,
+	nome varchar(255) not null,
+	prezzo int not null,
+	descrizione text not null,
+    immagine text not null,
+    vetrina boolean not null,
+    quantità int not null,
+    nomecategoria varchar(50) not null,
+    foreign key(nomecategoria) references categoria(nome));
+
+create table utente(
+	IdUtente int(10) zerofill auto_increment  primary key,
+    nome varchar(50) not null,
+    cognome varchar(50) not null,
+    email varchar(255) not null  unique,
+    passwordhash varchar(255) not null,
+    datadinascita date not null,
+    ruolo boolean not null
+    );
+
+create table itemCarrello(
+	IdProdotto int(5) zerofill,
+    #nomeProdotto varchar(50) not null,
+	#prezzoProdotto int not null,
+    #immagineProdotto text not null,
+    IdUtente int(10) zerofill,
+    quantità int not null,
+	primary key(IdProdotto, IdUtente),
+    foreign key(IdProdotto) references prodotto(IdProdotto)
+		on update cascade
+        on delete cascade,
+    foreign key(IdUtente) references utente(IdUtente) );
+
+
+create table metodospedizione (
+	nome varchar(50) not null primary key,
+	descrizione text not null,
+    costo int not null
+);
+
+create table ordine  (
+	IdOrdine int(10) zerofill auto_increment primary key,
+    totale int not null,
+    IdUtente int(10) zerofill,
+    cap int(5) zerofill not null,
+    numerocivico int not null, 
+    nome varchar(50) not null,
+    cognome varchar(50) not null,
+    via text not null,
+    telefono varchar(20),
+    nomemetodospedizione varchar(50) not null,
+    dataordine date not null,
+    foreign key(nomemetodospedizione) references metodospedizione(nome), 
+    foreign key(idutente) references utente(idutente));
+
+create table itemordine(
+	IdItemOrdine int(10) zerofill auto_increment primary key,
+    nome varchar(255) not null,
+    immagine text not null,
+    prezzo int not null,
+    quantità int not null,
+    IdProdotto int(5) zerofill,
+    IdOrdine int(10) zerofill not null,
+    foreign key(idprodotto) references prodotto(idprodotto)
+		on update cascade
+        on delete set null,
+    foreign key(idordine) references ordine(idordine)
+);
+
+create table recensione(
+	IdRecensione int(10) zerofill auto_increment primary key,
+    titolo varchar(255) not null,
+    descrizione text not null,
+    punteggio int not null,
+	IdProdotto int(5) zerofill,
+    idutente int(10) zerofill not null,
+    foreign key(idprodotto) references prodotto(idprodotto)
+		on update cascade
+        on delete set null,
+	foreign key(idutente) references utente(idutente)
+    );
+    
