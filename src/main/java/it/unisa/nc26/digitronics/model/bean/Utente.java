d8d03ad508bfbbe6b0f301fe72575822d076741b
Vincenzo2002 <dimiccovincenzo200230@gmail.com>
2024-12-27 11:39:47 +0100

    Creazione bean Utente e Recensione

---
+++ src/main/java/it/unisa/nc26/digitronics/model/bean/Utente.java
@@ -0,0 +1,81 @@
+package it.unisa.nc26.digitronics.model.bean;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
+
+public class Utente {
+    private int idUtente;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String passwordHash;
+    private LocalDate dataDiNascita;
+    private boolean ruolo;
+
+    public int getIdUtente() {
+        return idUtente;
+    }
+
+    public void setIdUtente(int idUtente) {
+        this.idUtente = idUtente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        try {
+            MessageDigest digest =
+                    MessageDigest.getInstance("SHA-1");
+            digest.reset();
+            digest.update(passwordHash.getBytes(StandardCharsets.UTF_8));
+            this.passwordHash = String.format("%040x", new BigInteger(1, digest.digest()));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public LocalDate getDataDiNascita() {
+        return dataDiNascita;
+    }
+
+    public void setDataDiNascita(LocalDate dataDiNascita) {
+        this.dataDiNascita = dataDiNascita;
+    }
+
+    public boolean isRuolo() {
+        return ruolo;
+    }
+
+    public void setRuolo(boolean ruolo) {
+        this.ruolo = ruolo;
+    }
+}
