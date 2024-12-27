58e234f14b62ad4ba8e492a26b892a728e83100f
Zoccola Domenico <d.zoccola3@studenti.unisa.it>
2024-12-27 11:39:35 +0100

    Commit bean Categoria, MetodoSpedizione,Prodotto e ConPool

---
+++ src/main/java/it/unisa/nc26/digitronics/model/bean/Categoria.java
@@ -0,0 +1,31 @@
+package it.unisa.nc26.digitronics.model.bean;
+
+public class Categoria {
+    private String nome;
+    private String descrizione;
+    private String immagine;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getImmagine() {
+        return immagine;
+    }
+
+    public void setImmagine(String immagine) {
+        this.immagine = immagine;
+    }
+}
