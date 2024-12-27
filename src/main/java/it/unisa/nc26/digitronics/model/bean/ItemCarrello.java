e8cde5a08d46e37c47070cdb4556e2715a76d093
Giovanni Paolo Chierchia <g.chierchia8@studenti.unisa.it>
2024-12-27 11:31:11 +0100

    Creazione bean ItemCarrello, ItemOrdine e Ordine

---
+++ src/main/java/it/unisa/nc26/digitronics/model/bean/ItemCarrello.java
@@ -0,0 +1,40 @@
+package it.unisa.nc26.digitronics.model.bean;
+
+public class ItemCarrello {
+    private Integer idUtente;
+    private int idProdotto;
+    private int quantità;
+    private Prodotto prodotto;
+
+    public Integer getIdUtente() {
+        return idUtente;
+    }
+
+    public void setIdUtente(Integer idUtente) {
+        this.idUtente = idUtente;
+    }
+
+    public int getIdProdotto() {
+        return idProdotto;
+    }
+
+    public void setIdProdotto(int idProdotto) {
+        this.idProdotto = idProdotto;
+    }
+
+    public int getQuantità() {
+        return quantità;
+    }
+
+    public void setQuantità(int quantità) {
+        this.quantità = quantità;
+    }
+
+    public Prodotto getProdotto() {
+        return prodotto;
+    }
+
+    public void setProdotto(Prodotto prodotto) {
+        this.prodotto = prodotto;
+    }
+}
