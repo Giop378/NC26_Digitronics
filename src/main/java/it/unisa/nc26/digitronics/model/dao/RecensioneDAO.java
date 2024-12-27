ea800edc5c0ccd3a06b4ab74aa3dacbb1dff5e4e
Vincenzo2002 <dimiccovincenzo200230@gmail.com>
2024-12-27 12:46:51 +0100

    Creazione UtenteDAO e RecensioneDAO

---
+++ src/main/java/it/unisa/nc26/digitronics/model/dao/RecensioneDAO.java
@@ -0,0 +1,64 @@
+package it.unisa.nc26.digitronics.model.dao;
+
+import it.unisa.nc26.digitronics.model.bean.Recensione;
+import it.unisa.nc26.digitronics.utils.ConPool;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class RecensioneDAO {
+
+    public void doSave(Recensione recensione) throws SQLException {
+        String sql = "INSERT INTO Recensione (Titolo, Descrizione, Punteggio, idUtente, idProdotto) VALUES(?, ?, ?, ?, ?)";
+
+        try(Connection con = ConPool.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+
+            ps.setString(1, recensione.getTitolo());
+            ps.setString(2, recensione.getDescrizione());
+            ps.setInt(3, recensione.getPunteggio());
+            ps.setInt(4, recensione.getIdUtente());
+            ps.setInt(5, recensione.getIdProdotto());
+
+            ps.executeUpdate();
+
+            try(ResultSet rs = ps.getGeneratedKeys()){
+                if(rs.next()){
+                    int id = rs.getInt(1);
+                    recensione.setIdRecensione(id);
+                }
+            }
+        }
+    }
+
+    public Collection<Recensione> doRetrieveByProduct(int product) throws SQLException{
+        String sql = "SELECT * FROM Recensione WHERE idProdotto = ?";
+        List<Recensione> recensioni = new ArrayList<>();
+
+        try(Connection con = ConPool.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql)){
+
+            ps.setInt(1, product);
+
+            try(ResultSet rs = ps.executeQuery()){
+
+                while (rs.next()){
+                    Recensione recensione = new Recensione();
+                    recensione.setIdRecensione(rs.getInt("idRecensione"));
+                    recensione.setTitolo(rs.getString("Titolo"));
+                    recensione.setDescrizione(rs.getString("Descrizione"));
+                    recensione.setPunteggio(rs.getInt("Punteggio"));
+                    recensione.setIdUtente(rs.getInt("idUtente"));
+                    recensione.setIdProdotto(rs.getInt("idProdotto"));
+                    recensioni.add(recensione);
+                }
+            }
+        }
+        return recensioni;
+    }
+
+}
+
+
---
+++ src/main/java/it/unisa/nc26/digitronics/model/dao/UtenteDAO.java
@@ -0,0 +1,80 @@
+package it.unisa.nc26.digitronics.model.dao;
+
+import it.unisa.nc26.digitronics.model.bean.Utente;
+import it.unisa.nc26.digitronics.utils.ConPool;
+
+import java.sql.*;
+
+public class UtenteDAO {
+
+    public Utente doRetrieveByEmailPassword(String email, String passwordhash) {
+
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        Utente utente = null;
+
+        try (Connection connection = ConPool.getConnection()) {
+            statement = connection.prepareStatement("SELECT idUtente, nome, cognome, email, passwordhash, datadinascita, ruolo FROM utente WHERE email=? AND passwordhash=SHA1(?)");
+            statement.setString(1, email);
+            statement.setString(2, passwordhash);
+            rs = statement.executeQuery();
+
+            if (rs.next()) {
+                utente = new Utente();
+                utente.setIdUtente(rs.getInt("idutente"));
+                utente.setNome(rs.getString("nome"));
+                utente.setCognome(rs.getString("cognome"));
+                utente.setEmail(rs.getString("email"));
+                utente.setPasswordHash(passwordhash);
+                utente.setDataDiNascita(rs.getDate("datadinascita").toLocalDate());
+                utente.setRuolo(rs.getBoolean("ruolo"));
+            }
+
+        }  catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs!= null) {
+                    rs.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return utente;
+    }
+
+    public synchronized int doSave(Utente utente) {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps = con.prepareStatement(
+                    "INSERT INTO utente (nome, cognome, email, passwordhash, datadinascita, ruolo) VALUES(?,?,?,?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            ps.setString(1, utente.getNome());
+            ps.setString(2, utente.getCognome());
+            ps.setString(3, utente.getEmail());
+            ps.setString(4, utente.getPasswordHash());
+            ps.setDate(5, java.sql.Date.valueOf(utente.getDataDiNascita()));
+            ps.setBoolean(6, utente.isRuolo());
+
+            if (ps.executeUpdate() != 1) {
+                throw new RuntimeException("INSERT error.");
+            }
+
+            try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    return generatedKeys.getInt(1);
+                } else {
+                    throw new RuntimeException("Failed to retrieve generated ID.");
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
